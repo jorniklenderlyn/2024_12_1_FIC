f7920bca1b26dd46f7c4c499f6464333dda69d96
jorniklenderlyn <khcfksiirjfldik@gmail.com>
2024-10-20 15:56:20 +1000

    docker

--- misc/predictor.py
+++ misc/predictor.py
@@ -1,21 +1,21 @@
-from catboost import CatBoostClassifier
-import joblib
-from sklearn.preprocessing import LabelEncoder
-import numpy as np 
+# from catboost import CatBoostClassifier
+# import joblib
+# from sklearn.preprocessing import LabelEncoder
+# import numpy as np 
 import pandas as pd
-import os
+# import os
 
 
-loaded_encoder = joblib.load(os.path.join(os.getcwd(), "models", "w20_10_6_35.pkl"))
+# loaded_encoder = joblib.load(os.path.join(os.getcwd(), "models", "w20_10_6_35.pkl"))
 
-model = CatBoostClassifier()
-model.load_model(os.path.join(os.getcwd(), "models", "w20_10_6_12.cbm"))
+# model = CatBoostClassifier()
+# model.load_model(os.path.join(os.getcwd(), "models", "w20_10_6_12.cbm"))
 
 
 def get_prediction(inference_dataframe: pd.DataFrame) -> list:
+    return [i for i in range(inference_dataframe.shape[0])]
     # y_pred = model.predict(inference_dataframe.drop('id',axis =1))
-    y_pred = model.predict(inference_dataframe)
-    y_pred_decoded = loaded_encoder.inverse_transform(y=y_pred)
-    answer = pd.DataFrame({'id': inference_dataframe['id'], 
-                           'target': y_pred_decoded})
-    return answer["target"].tolist()
+    # y_pred_decoded = loaded_encoder.inverse_transform(y=y_pred)
+    # answer = pd.DataFrame({'id': inference_dataframe['id'], 
+    #                        'target': y_pred_decoded})
+    # return answer["target"].tolist()
